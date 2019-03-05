0f6c1ea2f00e64df03d0568e5b77de31ae5df5a1
Matteo Kastler <matteo.kastler@gmail.com>
2019-03-05 22:06:02 +0100

    Add files via upload

---
+++ rewritten_spammer_code/rewritten_spammer_code.ino
@@ -0,0 +1,26 @@
+/* Author:Matteo Kastler
+ *  rewritten beacon frame spammer code 
+ *  for nodemcu V2 ESP8266 
+ *  in work: different modes(adcanced spam, random SSID spa),lcd display
+ */
+#include <ESP8266WiFi.h>
+
+
+void setup() {
+  delay(500);
+  wifi_set_opmode(STATION_MODE);
+  //monitormode for whatever reason
+  wifi_promiscuous_enable(1); 
+}
+
+void loop() {
+  //z.B. spambeacon("SSID");
+  
+
+}
+
+void advancedSpam(char[] ssid, int number){
+  
+  
+}
+
