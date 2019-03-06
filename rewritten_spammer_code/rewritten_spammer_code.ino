f91c341355a2b55e02aa37b3e29a2386fc5a308d
Matteo Kastler <matteo.kastler@gmail.com>
2019-03-06 22:12:25 +0100

    Update rewritten_spammer_code.ino

--- rewritten_spammer_code/rewritten_spammer_code.ino
+++ rewritten_spammer_code/rewritten_spammer_code.ino
@@ -5,19 +5,17 @@
  */
  
 #include <ESP8266WiFi.h>
-
-
 void setup() {
   delay(500);
   wifi_set_opmode(STATION_MODE);
   //monitormode for whatever reason
-  wifi_promiscuous_enable(1); 
+  wifi_promiscuous_enable(1);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  //z.B. spambeacon("SSID");
-  
-
+sendBeaconFrame("test");
 }
 
 /*void advancedSpam(char[] ssid, int number){
@@ -35,16 +33,15 @@ void sendBeaconFrame(char* ssid){
   wifi_set_channel(channel);
 
   uint8_t afterSSID[13] = {0x01, 0x08, 0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, //supported rate
-                        0x03, 0x01, 0x04};//channel?
+                        0x03, 0x01, 0x04}; //channel?
 
   uint8_t beacon[128] = {
                        0x80, 0x00, //Frame Control 
                        0x00, 0x00, //dauer
                /*4*/   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, //Zieladresse 
                /*10*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, //Senderadresse
-               /*16*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, //BSSID aka name des netzwerks
+               /*16*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, //SSID aka name des netzwerks
                /*22*/  0xc0, 0x6c, //Seq-ctl
-               //Fr
                /*24*/  0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, //timestamp- millisekunden Access point ist aktiv
                /*32*/  0xFF, 0x00, //Beacon interval
                /*34*/  0x01, 0x04, //irgendwas für staukontrolle oder so 
@@ -56,26 +53,47 @@ void sendBeaconFrame(char* ssid){
   beacon[37] = ssidLength;
 
   //ssid reinschreiben
-  for(int i; i < ssidLength; i++){
+  for(int i = 0; i < ssidLength; i++){
     beacon[38 + i] = ssid[i];
   }
   
-  //zeug nach ssid in das packet
+  //zeug nach ssid in das beacon
   for(int i = 0; i < 12; i++) {
       beacon[38 + ssidLength + i] = afterSSID[i];
   }
-  //
+  
   beacon[50 + ssidLength] = channel;
 
   //Sender Mac- adresse randomisieren
   for(int i = 10;i <= 15;i++){
       beacon[i] = beacon[i + 6] = random(256);
   }
-      
-  //packet,packetgröße,sequenznummern(true.false)
-  for(int i = 0; i < 5; i++){
+  //beacon,beacongröße,sequenznummern(true/false)
     wifi_send_pkt_freedom(beacon, beaconSize, 0);
-   }
-
-
+    wifi_send_pkt_freedom(beacon, beaconSize, 0);
+    wifi_send_pkt_freedom(beacon, beaconSize, 0);
+    delay(1);
 }
+void sendRandomBeacon(int length) {
+  char ssid[length+1];
+  randomString(length, ssid);
+  sendBeaconFrame (ssid);
+}
+
+void randomString(int length, char* ssid) {
+  String alfa = "1234567890qwertyuiopasdfghjkklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM_";
+  for(int i = 0; i < length; i++) {
+    ssid[i] = alfa[random(65)];
+  }
+}
+void rickRoll() {
+  sendBeaconFrame("01 Never gonna give you up,");
+  sendBeaconFrame("02 never gonna let you down");
+  sendBeaconFrame("03 Never gonna run around");
+  sendBeaconFrame("04  and desert you");
+  sendBeaconFrame("05 Never gonna make you cry,");
+  sendBeaconFrame("06 never gonna say goodbye");
+  sendBeaconFrame("07 Never gonna tell a lie");
+  sendBeaconFrame("08  and hurt you");
+}
+
